df3afbf788a39124c61bc5f29c6a3d5eea553ab3
Gaëtan H <gaetanhus@gmail.com>
2024-12-22 01:18:29 +0100

    feat(use-player.store.ts): add id state and setter to player store for enhanced player management feat(use-lang.tsx): implement persistence for language state using zustand middleware to retain user preference across sessions

--- apps/www/src/lib/hooks/store/use-player.store.ts
+++ apps/www/src/lib/hooks/store/use-player.store.ts
@@ -4,16 +4,20 @@ import { persist } from 'zustand/middleware';
 type PlayerStore = {
   playerName: string;
   setPlayerName: (name: string) => void;
+
+  id: string;
+  setId: (id: string) => void;
 }
 
 export const usePlayerStore = create<PlayerStore>()(
   persist(
     (set) => ({
-      playerName: '',
+      playerName: "",
       setPlayerName: (playerName: string) => set({ playerName }),
+
+      id: "",
+      setId: (id: string) => set({ id }),
     }),
-    {
-      name: 'player-storage',
-    }
+    { name: "player-storage", getStorage: () => localStorage }
   )
 );
