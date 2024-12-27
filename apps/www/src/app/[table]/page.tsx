cf22d350d972c70595399208d1ddd44e6347af7d
Gaëtan H <gaetanhus@gmail.com>
2024-12-27 01:57:08 +0100

    feat(table-page): add handling for WAITING_FOR_BETS game status and improve unknown status message

--- apps/www/src/app/[table]/page.tsx
+++ apps/www/src/app/[table]/page.tsx
@@ -3,14 +3,16 @@
 import { useBlackjack } from "@/lib/hooks/use-blackjack";
 import { WaitingPlayers } from "./_views/waiting-players.view";
 import { WaitingDistributes } from "./_views/waiting-distributes.view";
+import { WaitingBetting } from "./_views/waiting-betting";
 
 const TablePage = () => {
   const { gameStatus } = useBlackjack();
 
   if (gameStatus == "WAITING_FOR_PLAYERS") return <WaitingPlayers />;
+  if (gameStatus == "WAITING_FOR_BETS") return <WaitingBetting />;
   if (gameStatus == "WAITING_FOR_DISTRIBUTES") return <WaitingDistributes />;
   
-  return <WaitingDistributes />;
+  return <p>Unknown game status: {gameStatus}</p>;
 };
 
 export default TablePage;
