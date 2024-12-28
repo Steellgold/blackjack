b61cbb74d788d106358e208d8a998c810f9f108f
Gaëtan H <gaetanhus@gmail.com>
2024-12-28 21:03:52 +0100

    feat(blackjack.view.tsx): enhance BlackjackView component to display total hand values for dealer and player
    This change improves the user experience by providing clear visibility
    of the total hand values for both the dealer and the player, allowing
    for better gameplay understanding.

--- apps/www/src/app/[table]/_components/blackjack.view.tsx
+++ apps/www/src/app/[table]/_components/blackjack.view.tsx
@@ -8,6 +8,8 @@ import { useBlackjack } from "@/lib/hooks/use-blackjack";
 import { getHandValue } from "@blackjack/game/utils";
 import type { PropsWithChildren } from "react";
 import OtherPlayersCardsCard from "./other-players-cards";
+import { cn } from "@/lib/utils";
+import { BlackjackBadge } from "@/lib/components/ui/blackjack/blackjack-badge";
 
 export const BlackjackView: Component<PropsWithChildren> = ({ children }) => {
   const { players, cards: dealerCards, id } = useBlackjack();
@@ -15,18 +17,37 @@ export const BlackjackView: Component<PropsWithChildren> = ({ children }) => {
   const player = players.find(player => player.id === id);
   if (!player) return <div className="bg-red-500">Player not found !</div>;
 
+  const dealerHandValue = getHandValue(dealerCards);
+  const playerHandValue = getHandValue(player.cards);
+
   return (
     <div className="h-screen flex flex-col items-center justify-between p-3">
-      <UIBlackjackCard variant={getHandValue(dealerCards) > 21 ? "destructive" : "default"}>
-        <BlackjackCardsStack cards={dealerCards} playerId="DEALER" />
-      </UIBlackjackCard>
+      <div className="flex flex-col gap-0.5">
+        <UIBlackjackCard variant={dealerHandValue > 21 ? "destructive" : "default"} connected={dealerHandValue > 0 ? "to-bottom" : false}>
+          <BlackjackCardsStack cards={dealerCards} playerId="DEALER" />
+        </UIBlackjackCard>
+
+        {dealerHandValue > 0 && (
+          <UIBlackjackCard className="py-1 text-center" connected="to-top" variant={dealerHandValue > 21 ? "destructive" : "default"}>
+            Total: <span className="font-bold">{dealerHandValue}</span>
+          </UIBlackjackCard>
+        )}
+      </div>
       
       {children}
       
       <div className="flex flex-wrap gap-11">
-        <UIBlackjackCard key={player.id} variant={getHandValue(player.cards) > 21 ? "destructive" : "default"}>
-          <BlackjackCardsStack cards={player.cards} playerId={player.id} />
-        </UIBlackjackCard>
+        <div className="flex flex-col gap-0.5">
+          {playerHandValue > 0 && (
+            <UIBlackjackCard className="py-1 text-center" connected="to-bottom" variant={playerHandValue > 21 ? "destructive" : "default"}>
+              Total: <span className="font-bold">{playerHandValue}</span>
+            </UIBlackjackCard>
+          )}
+
+          <UIBlackjackCard key={player.id} variant={playerHandValue > 21 ? "destructive" : "default"} connected={playerHandValue > 0 ? "to-top" : false}>
+            <BlackjackCardsStack cards={player.cards} playerId={player.id} />
+          </UIBlackjackCard>
+        </div>
       </div>
 
       <UIBlackjackCard className="absolute bottom-4 right-4 p-2">
