648e41a98d906db7ef4c7a3afa354f4b1c473cdd
Gaëtan H <gaetanhus@gmail.com>
2024-12-28 21:03:12 +0100

    feat(blackjack-card): add connected prop to control card rounding based on connection state style(blackjack-card): update border color for destructive variant and clean up class names

--- apps/www/src/lib/components/ui/blackjack/blackjack-card.tsx
+++ apps/www/src/lib/components/ui/blackjack/blackjack-card.tsx
@@ -8,17 +8,28 @@ export type BlackjackCardVariant = "default" | "destructive" | "success" | "warn
 
 type BlackjackCardProps = React.HTMLAttributes<HTMLDivElement> & {
   variant?: BlackjackCardVariant;
+
+  connected?: false | "to-top" | "to-bottom";
 }
 
-export const BlackjackCard: Component<BlackjackCardProps> = ({ children, className, variant = "default", ...props }) => {
+export const BlackjackCard: Component<BlackjackCardProps> = ({
+  children,
+  className,
+  variant = "default",
+  connected = false,
+  ...props }) => {
   return (
     <div className={cn(
-      "bg-white bg-opacity-10 rounded-md border p-3 border-white border-opacity-20",
+      "bg-white bg-opacity-10 border p-3 border-white border-opacity-20",
       "transition-colors duration-300 ease-in-out",
       className, {
-        "bg-red-500 border-red-300": variant === "destructive",
+        "bg-red-500 border-red-600": variant === "destructive",
         "bg-green-500 border-green-300": variant === "success",
-        "bg-yellow-500 border-yellow-300": variant === "warning"
+        "bg-yellow-500 border-yellow-300": variant === "warning",
+
+        "rounded-md": !connected,
+        "rounded-t-md": connected === "to-bottom",
+        "rounded-b-md": connected === "to-top",
       }
     )} {...props}>
       {children}
